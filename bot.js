876f88aff0875107178a732900a141b4d00406b4
LUC4S1227 <43967172+LUC4S1227@users.noreply.github.com>
2018-10-28 18:38:43 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,6 +36,16 @@ client.on("message", async message => {
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const comando = args.shift().toLowerCase();
+  
+  // comando regras
+  if(comando === "regras") {
+   return message.reply("**Regras**
+❯ 1- ```Proibido compartilhar outros servidores de discord sem autorização prévia da moderação.
+❯ 2- Não falar qualquer tipo de palavão no chat.  :ok_hand: 
+❯ 3- O uso do flood e spam em nosso servidor são proibidos, pois além de "poluirem" o chat, podem, novamente, serem prejudiciais as pessoas que não desativam as notificações de nosso servidor.
+❯ 4- Não tente se passar por outras pessoas. :frowning:
+❯ 5- Proibido fazer propaganda de qualquer tipo.
+❯ 6- Pornografia leva a Ban.```")
 
   // comando equipe
   if(comando === "equipe") {
