3af3fcb9bb607b20de0ceb7c8d46b882a5db1b0e
LUC4S1227 <43967172+LUC4S1227@users.noreply.github.com>
2018-10-30 16:03:26 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+
+const client = new Discord.Client();
+client.prefix = config.prefix();
+
+client.on("iniciado",() => {
+  console.log("Bot iniciado!\n\nUsers: " + client.users.size + "\nServidores " + client.guilds.size);
+  client.user.setActivity(`${client.users.size} users`, {type: "Watching"});
+});
+
+ client.on("message", async message => {
+   let msg = message.content.toLowerCase();
+   if (message.author.bot) return undefined;
+
+   if (message.content.indexOf(client.prefix) !== 0) return;
+   const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+   const command = args.shift().toLowerCase();
+
+   try {
+      let commands = require(`./commands/${command}.js`);
+      commands.run(client, message, args);
+   } catch (e) {
+     console.log(e);
+   } finally {}
+
+ });
+
+client.login(config.token);
+const Discord = require('discord.js');
+const config = require("./config.json");
+
+const client = new Discord.Client();
+client.prefix = config.prefix();
+
+client.on("iniciado",() => {
+  console.log("Bot iniciado!\n\nUsers: " + client.users.size + "\nServidores " + client.guilds.size);
+  client.user.setActivity(`${client.users.size} users`, {type: "Watching"});
+});
+
+ client.on("message", async message => {
+   let msg = message.content.toLowerCase();
+   if (message.author.bot) return undefined;
+
+   if (message.content.indexOf(client.prefix) !== 0) return;
+   const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+   const command = args.shift().toLowerCase();
+
+   try {
+      let commands = require(`./commands/${command}.js`);
+      commands.run(client, message, args);
+   } catch (e) {
+     console.log(e);
+   } finally {}
+
+});
+
+client.login(config.token);
