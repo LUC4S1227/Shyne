93ba625e7d7a4b25429f39b91615ff8eb195953f
LUC4S1227 <43967172+LUC4S1227@users.noreply.github.com>
2018-10-30 16:14:24 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,7 +6,17 @@ client.prefix = config.prefix();
 
 client.on("iniciado",() => {
   console.log("Bot iniciado!\n\nUsers: " + client.users.size + "\nServidores " + client.guilds.size);
-  client.user.setActivity(`${client.users.size} users`, {type: "Watching"});
+  client.user.setActivity(`Shyne - Um servidor para se divertir com amigos, além de conhecer pessoas novas! Vários chats e canais de voz de diferentes jogos!`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(`O bot entrou nos servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
+  client.user.setActivity(`Shyne - Um servidor para se divertir com amigos, além de conhecer pessoas novas! Vários chats e canais de voz de diferentes jogos!`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Shyne - Um servidor para se divertir com amigos, além de conhecer pessoas novas! Vários chats e canais de voz de diferentes jogos!`);
 });
 
  client.on("message", async message => {
@@ -24,7 +34,7 @@ client.on("iniciado",() => {
      console.log(e);
    } finally {}
 
- });
+});
 
 client.login(config.token);
 const Discord = require('discord.js');
