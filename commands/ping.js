1bc41149abd5b98867854e344a9c4611c2d535fc
LUC4S1227 <43967172+LUC4S1227@users.noreply.github.com>
2018-10-30 16:25:01 -0300

    Delete ping.js

--- commands/ping.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client, message, args) => {
-  const m = await message.channel.send("Calculando...");
-  m.edit(`Pong! :ping_pong: \nLatencia é**${m.createdTimestamp - message.createdTimestamp}ms** \nLatencia API é **${client.ping}ms**`);
-}
